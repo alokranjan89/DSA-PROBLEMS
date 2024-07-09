eb2265b3c58f6d1de1e7b6d2c6790dfbd6d13a4d
Alok Ranjan <alokr5393@gmail.com>
2024-07-09 10:18:01 +0530

    Linkedlist

---
+++ Linkedlist/firstOccuringInDll.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node()
+    {
+        this->data = 0;
+        next = NULL;
+    }
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+    Node(int data, Node *next)
+    {
+        this->data = data;
+        this->next = next;
+    }
+};
+
+Node *firstNode(Node *head)
+{
+    //    Write your code here.
+    Node *slow = head;
+    Node *fast = head;
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast)
+        {
+            slow = head;
+            while (slow != fast)
+            {
+                slow = slow->next;
+                fast = fast->next;
+            }
+            return slow;
+        }
+    }
+    return NULL;
+}
